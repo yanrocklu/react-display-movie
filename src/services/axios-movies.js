5fb3f10b6fd7ea41d2bf7cb2ba415250dd5d5ddd
Yan Lu <yanrocklu@gmail.com>
2019-10-20 23:29:53 -0400

    updated README

--- src/services/axios-movies.js
+++ src/services/axios-movies.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 // create an axios instance
 const axiosInstance = axios.create({
+  // fetch data from Firebase
   baseURL: 'https://react-display-movie-c4fc7.firebaseio.com/'
 });
 
